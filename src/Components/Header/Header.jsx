c186bd6a72f6ef3743c061a8fa95cf10ae5e8067
Eduardo Mangabeira <eduardomangabeiraneto@gmail.com>
2024-05-05 23:59:37 -0300

    Projeto Landing Page Banco Itaú

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import * as S from "./styles";
+
+import ItemMenu from "./ItemMenu";
+
+import LogoItau from "../../assets/logo.svg";
+import IconUser from "../../assets/icon-user.svg";
+
+
+export default function Header() {
+  return (
+    <S.Header>
+      <S.Container>
+        <S.ContainerItens>
+          <div>
+            <S.Img src={LogoItau} alt="Logo itaú" />
+          </div>
+
+          <S.Ul>
+            <S.Li>
+              <ItemMenu name="Para você" />
+            </S.Li>
+            <S.Li>
+              <ItemMenu name="Para empresas" />
+            </S.Li>
+            <S.Li>
+              <ItemMenu name="Serviços" />
+            </S.Li>
+            <S.Li>
+              <ItemMenu name="Ajuda" />
+            </S.Li>
+          </S.Ul>
+
+          <S.DivButtonAccess>
+            <S.ButtonAccess>
+              <img src={IconUser} alt="Icon user" />
+              <span>Acessar Conta</span>
+            </S.ButtonAccess>
+          </S.DivButtonAccess>
+        </S.ContainerItens>
+      </S.Container>
+    </S.Header>
+  );
+}
---
+++ src/Components/SectionService/ItemList.jsx
@@ -0,0 +1,14 @@
+import * as S from "./styles";
+
+
+export default function ItemList ({p, img, alt}){
+  return (
+    <>
+      <S.DivMascara>
+        <img src={img} alt={alt} />
+      </S.DivMascara>
+
+      <p>{p}</p>
+    </>
+  );
+}
